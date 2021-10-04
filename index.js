8595e618a263fb0beeb449abfce1cc1fc7037d1b
Nameer Rizvi <rizvinameer@gmail.com>
2021-10-04 16:54:10 -0400

    Logic fixes, package updates

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ if (DOMPurify.sanitize) {
     sanitizer = (dirty, options) =>
       decode(DOMPurifyWindow.sanitize(dirty, options));
   } catch (error) {
-    console.error("[sanitized] " + error.toString());
+    console.error(error);
   }
 }
 
-function handleDirtyValue(dirty, DOMPurifyOptions, callback) {
+function sanitize(dirty, DOMPurifyOptions, callback) {
   try {
-    if (dirty && dirty.constructor === String)
-      return sanitizer(dirty, DOMPurifyOptions);
+    if (typeof dirty === "string")
+      return sanitizer(dirty, DOMPurifyOptions, callback);
 
     if (dirty && dirty.constructor === Array) {
       let clone = [].concat(dirty);
       for (let i = 0; i < clone.length; i++) {
-        clone[i] = handleDirtyValue(clone[i], DOMPurifyOptions);
+        clone[i] = sanitize(clone[i], DOMPurifyOptions, callback);
       }
       return clone;
     }
@@ -33,18 +33,21 @@ function handleDirtyValue(dirty, DOMPurifyOptions, callback) {
     if (dirty && dirty.constructor === Object) {
       let clone = JSON.parse(JSON.stringify(dirty));
       let cloneKeys = Object.keys(clone);
-      for (let j = 0; j < cloneKeys.length; j++) {
-        const cloneKey = cloneKeys[j];
-        clone[cloneKey] = handleDirtyValue(clone[cloneKey], DOMPurifyOptions);
+      for (let i = 0; i < cloneKeys.length; i++) {
+        const cloneKey = cloneKeys[i];
+        clone[cloneKey] = sanitize(clone[cloneKey], DOMPurifyOptions, callback);
       }
       return clone;
     }
 
+    if (callback) callback(null, dirty);
+
     return dirty;
   } catch (err) {
     if (callback) callback(err);
+
     return dirty;
   }
 }
 
-module.exports = handleDirtyValue;
+module.exports = sanitize;
