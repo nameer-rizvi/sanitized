af03b696c35cd6c15de0bf1d03297a4e27e951c8
nameer-rizvi <rizvinameer@gmail.com>
2023-01-10 14:02:21 -0500

    package update, refactoring, change to yarn

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-const DOMPurify = require("dompurify");
+let dompurify = require("dompurify");
 const he = require("he");
 
-let sanitizer = (dirty) => dirty;
-
-if (DOMPurify.sanitize) {
-  sanitizer = (dirty, options) => he.decode(DOMPurify.sanitize(dirty, options));
-} else {
-  try {
-    const jsdom = require("jsdom");
-    const JSDOM = new jsdom.JSDOM("<!DOCTYPE html>");
-    const DOMPurifyWindow = DOMPurify(JSDOM.window);
-    sanitizer = (dirty, options) =>
-      he.decode(DOMPurifyWindow.sanitize(dirty, options));
-  } catch (error) {
-    console.error(error);
-  }
+if (!dompurify.sanitize) {
+  const jsdom = require("jsdom");
+  const jsdomWindow = new jsdom.JSDOM("").window;
+  dompurify = dompurify(jsdomWindow);
 }
 
-function sanitized(dirty, DOMPurifyOptions, errorCallback) {
-  try {
-    let clone = JSON.parse(JSON.stringify(dirty));
+function sanitized(dirty = "", dompurifyOption) {
+  let clone = JSON.parse(JSON.stringify(dirty));
 
-    if (typeof clone === "string") clone = sanitizer(clone, DOMPurifyOptions);
-
-    if (clone instanceof Array)
-      for (let i = 0; i < clone.length; i++)
-        clone[i] = sanitized(clone[i], DOMPurifyOptions);
-
-    if (clone instanceof Object)
-      for (let cloneKey of Object.keys(clone))
-        clone[cloneKey] = sanitized(clone[cloneKey], DOMPurifyOptions);
-
-    return clone;
-  } catch (err) {
-    if (errorCallback) errorCallback(err);
-
-    return dirty;
+  if (typeof clone === "string") {
+    clone = he.decode(dompurify.sanitize(dirty, dompurifyOption));
+  } else if (clone instanceof Array) {
+    for (let i = 0; i < clone.length; i++)
+      clone[i] = sanitized(clone[i], dompurifyOption);
+  } else if (clone instanceof Object) {
+    for (let key of Object.keys(clone))
+      clone[key] = sanitized(clone[key], dompurifyOption);
   }
+
+  return clone;
 }
 
 module.exports = sanitized;
