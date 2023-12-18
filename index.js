8280279c920f5dab0c6903b1c85db345151d0bcc
nameer-rizvi <rizvinameer@gmail.com>
2023-12-18 18:09:28 -0500

    packages update

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const he = require("he");
 
 if (!dompurify.sanitize) {
   const jsdom = require("jsdom");
+
   const jsdomWindow = new jsdom.JSDOM("").window;
+
   dompurify = dompurify(jsdomWindow);
 }
 
 function sanitized(dirty = "", dompurifyOption) {
   let clone = JSON.parse(JSON.stringify(dirty));
 
-  if (typeof clone === "string") {
-    clone = he.decode(dompurify.sanitize(dirty, dompurifyOption));
-  } else if (clone instanceof Array) {
-    for (let i = 0; i < clone.length; i++)
+  if (clone instanceof Array) {
+    for (let i = 0; i < clone.length; i++) {
       clone[i] = sanitized(clone[i], dompurifyOption);
+    }
   } else if (clone instanceof Object) {
-    for (let key of Object.keys(clone))
+    for (let key of Object.keys(clone)) {
       clone[key] = sanitized(clone[key], dompurifyOption);
+    }
+  } else if (typeof clone === "string") {
+    clone = he.decode(dompurify.sanitize(dirty, dompurifyOption));
   }
 
   return clone;
